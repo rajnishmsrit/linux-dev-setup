5b83688829ee8d39c94f69652b9b324a3d2cfbb4
Rajnish Kumar <rajnishmsrit@gmail.com>
2019-03-25 02:26:14 +0530

    Added sublime config

---
+++ sublime/sublimeTextEditor3.sh
@@ -0,0 +1,20 @@
+#config of xdebug in php.ini
+#setup sublime user text editor with profile info as 
+#
+#
+#
+: '
+	{
+	// Which port number Sublime Text should listen
+    // to connect with debugger engine.
+    "port": 9001,
+
+    // Show information about super globals in context view.
+    "super_globals": false,
+
+    // Show detailed log information about communication
+    // between debugger engine and Sublime Text.
+    // Log can be found at Packages/User/Xdebug.log
+    "debug": true
+	}
+'
